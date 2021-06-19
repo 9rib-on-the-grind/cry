60c6da99b9fc3970c416cafcb839b8d19ad91629
pavuk <mnl.xpq@gmail.com>
2021-06-19 20:22:43 +0300

    Add candlestick visualization utility

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 
 from dotenv import load_dotenv
 from binance import Client
+import pandas as pd
+
+from trainer import Trainer
+import visualizer
 
 
 load_dotenv()
@@ -10,3 +14,12 @@ api_key = os.getenv('READONLY_API_KEY')
 secret_key = os.getenv('READONLY_SECRET_KEY')
 
 client = Client(api_key, secret_key)
+trainer = Trainer(client)
+
+df = trainer.build_dataset(
+					symbol='BTCUSDT', 
+					interval='1m', 
+					period='1 hour',
+	)
+
+visualizer.show_graph(df)
---
+++ visualizer.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import mplfinance as mpf
+
+from mplfinance.original_flavor import candlestick_ohlc
+import matplotlib.dates as mpdates
+
+
+def show_graph(df=None, file=None):
+	if df is not None or file is not None:
+		if file is not None:
+			df = pd.read_csv(file, dtype=float)
+		df = df[['Open time', 'Open', 'High', 
+		         'Low', 'Close']]
+		df['Open time'] = range(len(df))
+
+		plt.style.use('dark_background')
+		fig, ax = plt.subplots()
+		ax.grid(color='grey', linestyle='dashed', linewidth=.5, alpha=.5)
+		  
+		candlestick_ohlc(ax, df.values, width = 0.6,
+		                 colorup = 'green', colordown = 'red', 
+		                 alpha = 0.9)
+		
+		ax.set_xlabel('Time')
+		ax.set_ylabel('Price')
+		fig.tight_layout()
+
+		plt.show()
+
+
