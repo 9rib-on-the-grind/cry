60b52ebbd1aab2960f0f6b10b14b9657b1d90fa0
pavuk <mnl.xpq@gmail.com>
2021-06-23 04:08:13 +0300

    Simplest model check

--- code/model.py
+++ code/model.py
@@ -2,17 +2,31 @@ import tensorflow as tf
 from tensorflow import keras
 
 
+# class CandlestickCNN(keras.Model):
+# 	def __init__(self, **kwargs):
+# 		super(Model, self).__init__(**kwargs)
+
+
+# class Model(keras.Model):
+# 	def __init__(self, **kwargs):
+# 		super(Model, self).__init__(**kwargs)
+# 		self.modules = [
+# 			CandlestickCNN(),
+# 		]
 
 
 
-class CandlestickCNN(keras.Model):
-	def __init__(self, **kwargs):
-		super(Model, self).__init__(**kwargs)
+def get_model():
+	model = keras.models.Sequential()
 
+	model.add(keras.layers.Conv1D(32, 5, padding='same', input_shape=(20, 6)))
+	model.add(keras.layers.GlobalAveragePooling1D())
+	model.add(keras.layers.Flatten())
+	model.add(keras.layers.Dense(2))
 
-class Model(keras.Model):
-	def __init__(self, **kwargs):
-		super(Model, self).__init__(**kwargs)
-		self.modules = [
-			CandlestickCNN(),
-		]
+	model.compile(
+			optimizer='SGD',
+			loss='mse'
+		)
+
+	return model
