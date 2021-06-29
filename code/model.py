ae800bc0c8924d42da10280eeac6e3c63ad5133e
pavuk <mnl.xpq@gmail.com>
2021-06-29 05:22:42 +0300

    CNN model replaced with simple RNN model

--- code/model.py
+++ code/model.py
@@ -4,40 +4,21 @@ import numpy as np
 
 
 def get_model():
-	model = Model()
-	model.compile(
-			optimizer=keras.optimizers.SGD(learning_rate=.1, momentum=.9, nesterov=True),
-			loss='mse',
-		)
-	return model
+    model = get_candlestick_rnn_model()
+    model.compile(
+            optimizer=keras.optimizers.SGD(learning_rate=.1, momentum=.9, nesterov=True),
+            loss='mse',
+        )
+    return model
 
 
 
+def get_candlestick_rnn_model():
+    model = keras.models.Sequential()
 
-class CandlestickCNNModule(keras.layers.Layer):
-	def __init__(self, **kwargs):
-		super(CandlestickCNNModule, self).__init__(**kwargs)
+    model.add(keras.layers.LSTM(50, recurrent_dropout=.5, return_sequences=True))
+    model.add(keras.layers.LSTM(50, recurrent_dropout=.5, return_sequences=False))
+    model.add(keras.layers.Dense(1))
 
-		self.main_layers = [
-			keras.layers.Conv1D(32, 5, padding='same'),
-			keras.layers.GlobalAveragePooling1D(),
-			keras.layers.Flatten(),
-			keras.layers.Dense(6),
-		]
+    return model
 
-
-	def call(self, inputs):
-		for layer in self.main_layers:
-			inputs = layer(inputs)
-		return inputs
-
-
-class Model(keras.Model):
-	def __init__(self, **kwargs):
-		super(Model, self).__init__(**kwargs)
-
-		self.candlestick_module = CandlestickCNNModule()
-
-
-	def call(self, inputs):
-		return self.candlestick_module(inputs)
