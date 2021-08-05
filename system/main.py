46b5497d4c007f4474c3c0ec791bd3adf283bea0
pavuk <mnl.xpq@gmail.com>
2021-08-05 15:01:58 +0300

    Remove simulation.py

--- system/main.py
+++ system/main.py
@@ -1,5 +1,3 @@
-import simulation
-
 import pandas as pd
 import numpy as np
 
@@ -10,8 +8,4 @@ pd.set_option('display.max_columns', 100)
 pd.set_option('display.width', 1000)
 
 np.set_printoptions(edgeitems=10, linewidth=200)
-pd.options.mode.chained_assignment = None 
-
-
-
-simulation.simulate()
+pd.options.mode.chained_assignment = None
