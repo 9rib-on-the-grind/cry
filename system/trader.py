7192fe36a64f5a737a7d4f12ba58b456bc1d6791
pavuk <mnl.xpq@gmail.com>
2021-08-07 07:56:30 +0300

    Add timeframes information & chage time for trades to unix time

--- system/trader.py
+++ system/trader.py
@@ -2,6 +2,7 @@ import time
 from collections.abc import Mapping, Iterable
 
 import experts
+import data
 
 
 
@@ -9,16 +10,20 @@ class BaseTrader:
     def __init__(self):
         pass
 
-    def set_data(self, data):
+    def set_data(self, data: data.DataMaintainer):
+        assert hasattr(self, 'expert'), 'PairExpert is not set'
         self.data = data
+        self.expert.set_data(data)
 
 
 
 class PairTrader(BaseTrader):
-    def __init__(self):
+    def __init__(self, pair: str):
+        self.pair = pair
         self.balance = self.initial_money = 100
         self.quantity = 0
-        self.trashold = .5
+        self.trashold = .2
+        self.estimations = []
         self.trades = []
         self.profit = []
         self.time, self.times = 0, []
@@ -26,6 +31,8 @@ class PairTrader(BaseTrader):
 
     def set_expert(self, expert: experts.PairExpert):
         self.expert = expert
+        self.timeframes = [expert.timeframe for expert in self.expert._inner_experts]
+        self.min_timeframe = self.timeframes[-1]
 
     def update(self, data: Mapping[str, Iterable]):
         """Update candlesticks data, update expert.
@@ -40,21 +47,23 @@ class PairTrader(BaseTrader):
             self.data[timeframe, 'History'].set_update_hash(time.time())
         self.expert.update()
 
-    def act(self, timeframe):
-        estimation = self.expert.estimate()
+    def act(self):
+        timeframe =  self.min_timeframe
         price = self.data[timeframe, 'History', 'Close'][-1]
+        time = self.data[timeframe, 'History', 'Close time'][-1]
+        estimation = self.expert.estimate()
+        self.estimations.append(estimation)
         if estimation > self.trashold and self.balance > 0: # buy
             self.quantity = (1 - self.commision) * self.balance / price
-            self.times.append(self.time)
+            self.times.append(time)
             self.trades.append(('buy', self.quantity, price))
             self.balance = 0
         elif estimation < -self.trashold and self.quantity > 0: # sell
             self.balance = (1 - self.commision) * self.quantity * price
-            self.times.append(self.time)
+            self.times.append(time)
             self.trades.append(('sell', self.quantity, price))
             self.profit.append(self.evaluate_profit())
             self.quantity = 0
-        self.time += 1
 
     def show_evaluation(self):
         if self.quantity > 0:
